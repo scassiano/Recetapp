bb46ac30ca4585c2379002cef384e457e8063901
Santiago Cassiano Rozo <scassiano@unal.edu.co>
2023-10-21 10:46:51 -0500

    Cambio logo y titulo

--- Recetapp/src/pages/Home.tsx
+++ Recetapp/src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Recetapp</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
